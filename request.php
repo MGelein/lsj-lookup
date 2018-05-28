e159a537792a05f64153fe280caec0b750ce3ec1
Mees Gelein <meesgelein@gmail.com>
2018-05-28 22:09:47 +0200

    Optimizing the request

--- request.php
+++ request.php
@@ -3,3 +3,25 @@
  * Requests the content for a single entry
  */
 header('Content-type:text/plain; charset=UTF-8');
+if(!isset($_GET['id'])){
+    echo "No ID was provided";
+    exit();
+}
+$id = $_GET['id'];
+
+//Get a handle to the file and read through it
+$handle = fopen('data/lsj-combined.cex', 'r');
+$idNr = str_replace('n', '', $id);
+$idNr -= 100;
+//Read every line
+while(($line = fgets($handle)) !== false) {
+    //Skip this line, we're not even close
+    $idNr --;
+    if($idNr > 0) continue;
+    //Now we're getting close, read every line
+    if(substr($line, 0, strpos($line, '#')) == $id){
+        echo $line;
+        exit();
+    }
+}
+exit(); 
