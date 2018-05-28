f418c76370a33b4bb16e53c4b572a32dc3c6b435
Mees Gelein <meesgelein@gmail.com>
2018-05-28 16:27:34 +0200

    Working on the service

--- search.php
+++ search.php
@@ -3,7 +3,7 @@
  * This simple script will look for the provided search query and return the id's of the 
  * objects that match in their lemma
  */
-header('Content-type:text/plain; charset=UTF-8');
+header('Content-type:application/json; charset=UTF-8');
 //See if any query data is set
 if(!isset($_GET['q'])){
     echo "What are you doing here?";
@@ -13,15 +13,24 @@ if(!isset($_GET['q'])){
 //Get the query data from the GET url variable
 $query = $_GET['q'];
 
+//Start of the JSON response
+echo '{';
+
 //Read through the file using an iterator
 $handle = fopen('data/lsj-combined.cex', 'r');
 //Read every line
+echo '"ids":[';
 while(($line = fgets($handle)) !== false) {
     //Only echo back the ones we need
     if(strpos(strtolower($line), strtolower($query)) !== FALSE){
-        echo substr($line, 0, strpos($line, '#')) . ',';
+        echo '"' . substr($line, 0, strpos($line, '#')) . '",';
     }
 }
+//Close the array
+echo '""]';
 
 //Close the file again
 fclose($handle);
+
+//End of JSON response
+echo '}';
